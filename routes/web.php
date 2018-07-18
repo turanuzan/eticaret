36db5a8a4c2344cbbe798074ab405217816c8c50
topcueser <topcueser@gmail.com>
2018-07-18 14:55:40 +0300

    Ide Helper eklendi ve bazı değişiklikler yapıldı

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/merhaba', function (){
+    return "Merhaba";
+});
+
+Route::get('/urun/{urun_adi}/{id?}',function ($urun_adi,$id = 0){
+    return "Toplam : $id $urun_adi";
+})->name('urun_detay');
+
+Route::get('/kampanya',function (){
+    return redirect()->route('urun_detay',['urun_adi' => 'elma','id' => 5]);
+});
