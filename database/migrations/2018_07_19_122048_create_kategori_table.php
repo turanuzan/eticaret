2d68c35d23abf6be4843fb789da1f93e4ebd0570
topcueser <topcueser@gmail.com>
2018-07-19 15:33:46 +0300

    migrate

---
+++ database/migrations/2018_07_19_122048_create_kategori_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKategoriTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kategori', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('kategori_adi',30);
+            $table->string('slug',40);
+            //$table->timestamps(); // created_at ve updated_at olarak kendi iki tane kolon acar.
+            $table->timestamp('olusturma_tarihi')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('guncelleme_tarihi')->default(DB::raw('CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kategori');
+    }
+}
