415e68a634d1b42f75d43e8a9d0120904e0414c5
topcueser <topcueser@gmail.com>
2018-07-19 17:48:55 +0300

    urun sayfası dinamikleştirildi

--- app/Http/Controllers/UrunController.php
+++ app/Http/Controllers/UrunController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Urun;
 use Illuminate\Http\Request;
 
 class UrunController extends Controller
 {
-    public function index()
+    public function index($slug_urunadi)
     {
-        return view('urun');
+        //$urun = Urun::where('slug',$slug_urunadi)->firstOrFail();
+        $urun = Urun::whereSlug($slug_urunadi)->firstOrFail();
+
+        // belongsToMany ozelligi ile urunler modelinden kategoriler geldi
+        // aynı ürün aynı kategoride birden fazla varsa tekilleştirmek adına distinct yaptık
+        $kategoriler = $urun->kategoriler()->distinct()->get();
+
+        return view('urun',compact('urun','kategoriler'));
     }
 }
