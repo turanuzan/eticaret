b145c3ed5094ec9aa2750dfb94d118b611fe6e11
topcueser <topcueser@gmail.com>
2018-07-21 18:28:41 +0300

    sepetteki ürünleri ekleme,silme,guncelleme ozelliklerinin veritabanına da yansıtılması

--- app/Http/Controllers/SepetController.php
+++ app/Http/Controllers/SepetController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sepet;
+use App\Models\SepetUrun;
 use App\Models\Urun;
 use Gloudemans\Shoppingcart\Facades\Cart; // dışarıdan composer ile eklenen kütüphane
 use Illuminate\Http\Request;
@@ -17,7 +19,23 @@ class SepetController extends Controller
     public function ekle()
     {
         $urun = Urun::find(request('id'));
-        Cart::add($urun->id,$urun->urun_adi,1,$urun->fiyati,['slug' => $urun->slug]); // son parametrede ekstra alan gonderebiliriz.
+        $cartItem = Cart::add($urun->id,$urun->urun_adi,1,$urun->fiyati,['slug' => $urun->slug]); // son parametrede ekstra alan gonderebiliriz.
+
+        if(auth()->check()){ // kullanıcı girişi olup olmadığını kontrol etmektedir. Kullanıcı girişi yapanlar için geçerli
+            $aktif_sepet_id = session('aktif_sepet_id');
+            if(!isset($aktif_sepet_id)){
+                $aktif_sepet = Sepet::create([
+                    'kullanici_id' => auth()->id()
+                ]);
+                $aktif_sepet_id = $aktif_sepet->id;
+                session()->put('aktif_sepet_id',$aktif_sepet_id);
+            }
+
+            SepetUrun::updateOrCreate(
+                ['sepet_id' => $aktif_sepet_id, 'urun_id' => $urun->id], // where şartı gibi varsa gunceller yoksa yeni ekler.
+                ['adet' => $cartItem->qty, 'tutar' => $urun->fiyati, 'durum' => 'Beklemede']
+            );
+        }
 
         // bu şekilde with ile mesaj gonderebiliriz.Diğer kullanımı şekli KullaniciController da var
         return redirect()->route('sepet')
@@ -25,9 +43,16 @@ class SepetController extends Controller
             ->with('mesaj','Ürün sepete eklendi.');
     }
 
-    public function kaldir($rowid)
+    public function kaldir($rowId)
     {
-        Cart::remove($rowid);
+        if(auth()->check()){ // kullanıcı girişi olup olmadığını kontrol etmektedir. Kullanıcı girişi yapanlar için geçerli
+            $aktif_sepet_id = session('aktif_sepet_id');
+            $cartItem = Cart::get($rowId); // verilen id ye gore o satırdaki bütün verilere ulaşılabilir.
+            // delete() burada softdelete yapacak yani tabloda silinme tarihi alanını doldurur ve sorgularda gelmeyecektir.
+            SepetUrun::where('sepet_id',$aktif_sepet_id)->where('urun_id',$cartItem->id)->delete();
+        }
+
+        Cart::remove($rowId);
         return redirect()->route('sepet')
             ->with('mesaj_tur','success')
             ->with('mesaj','Ürün sepetten kaldırıldı.');
@@ -35,6 +60,11 @@ class SepetController extends Controller
 
     public function bosalt()
     {
+        if(auth()->check()){ // kullanıcı girişi olup olmadığını kontrol etmektedir. Kullanıcı girişi yapanlar için geçerli
+            $aktif_sepet_id = session('aktif_sepet_id');
+            SepetUrun::where('sepet_id',$aktif_sepet_id)->delete();
+        }
+
         Cart::destroy();
         return redirect()->route('sepet')
             ->with('mesaj_tur','success')
@@ -56,6 +86,19 @@ class SepetController extends Controller
             session()->flash('mesaj_tur','danger');
             session()->flash('mesaj','Adet değeri 1 ile 5 arasında olmalıdır.');
             return response()->json(['success' => false]);
+        }
+
+        if(auth()->check()){
+            $aktif_sepet_id = session('aktif_sepet_id');
+            $cartItem = Cart::get($rowId);
+
+            if(request('adet') > 0){
+                SepetUrun::where('sepet_id',$aktif_sepet_id)
+                    ->where('urun_id',$cartItem->id)
+                    ->update(['adet' => request('adet')]);
+            }else{ // adet 0 gelirse Cart kütüphanesi kendisinden kaldırıyor bizimde db den kaldırmamız gerekiyor.
+                SepetUrun::where('sepet_id',$aktif_sepet_id)->where('urun_id',$cartItem->id)->delete();
+            }
         }
 
         Cart::update($rowId,request('adet'));
