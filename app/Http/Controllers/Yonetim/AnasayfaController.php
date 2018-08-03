b85d6c6a6ae3d2a5b6c5d70f61e9ce9b4750291a
topcueser <topcueser@gmail.com>
2018-08-03 16:19:01 +0300

    file cache kullanımı

--- app/Http/Controllers/Yonetim/AnasayfaController.php
+++ app/Http/Controllers/Yonetim/AnasayfaController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers\Yonetim;
 
+use App\Models\Siparis;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
 
 class AnasayfaController extends Controller
 {
     public function index()
     {
-        return view('yonetim.anasayfa');
+        $istatistikler = Cache::get('istatistikler');
+
+        //if(!Cache::has('istatistikler')){} // bu şekilde de bir kontrol yapabilirdik ama o zaman else kısmında Cache::get('istatistikler'); demek durumunda kalırız.
+        /*if(empty($istatistikler)){
+
+            $istatistikler = [
+                'bekleyen_siparis' => Siparis::where('durum','Siparişiniz alındı')->count()
+            ];
+
+            $bitisZamani = now()->addMinutes(10); // 10 dk lık zaman dilimi olarak ayarladık.
+
+            Cache::put('istatistikler', $istatistikler, $bitisZamani); // her zaman eski veriyi siler yenisini ekler.
+            //Cache::add('istatistikler', $istatistikler, $bitisZamani); // cache üzerinde var ise yeniyi eklemez
+        }*/
+
+        //Cache::forget('istatistikler'); // o keye ait cache siler
+        //Cache::flush(); // tüm cache temizler
+
+        // yukarıdaki bütün kontrollerin yanında daha kullanışlı yol ise
+
+        $bitisZamani = now()->addMinutes(10);
+
+        $istatistikler = Cache::remember('istatistikler',$bitisZamani,function (){ // varsa getirir yoksa yenisini alır.
+            return [
+                'bekleyen_siparis' => Siparis::where('durum','Siparişiniz alındı')->count()
+            ];
+        });
+
+        return view('yonetim.anasayfa',compact('istatistikler'));
     }
 }
--- resources/views/yonetim/anasayfa.blade.php
+++ resources/views/yonetim/anasayfa.blade.php
@@ -7,37 +7,36 @@
     <section class="row text-center placeholders">
         <div class="col-6 col-sm-3">
             <div class="panel panel-primary">
-                <div class="panel-heading">Header</div>
+                <div class="panel-heading">Bekleyen Sipariş</div>
                 <div class="panel-body">
-                    <h4>123</h4>
-                    <p>Data</p>
+                    <h4>{{ $istatistikler['bekleyen_siparis'] }}</h4>
                 </div>
             </div>
         </div>
         <div class="col-6 col-sm-3">
             <div class="panel panel-primary">
-                <div class="panel-heading">Header</div>
+                <div class="panel-heading">Tamamlanan Sipariş</div>
                 <div class="panel-body">
                     <h4>123</h4>
-                    <p>Data</p>
+                    <p>Adet</p>
                 </div>
             </div>
         </div>
         <div class="col-6 col-sm-3">
             <div class="panel panel-primary">
-                <div class="panel-heading">Header</div>
+                <div class="panel-heading">Ürün</div>
                 <div class="panel-body">
                     <h4>123</h4>
-                    <p>Data</p>
+                    <p>Adet</p>
                 </div>
             </div>
         </div>
         <div class="col-6 col-sm-3">
             <div class="panel panel-primary">
-                <div class="panel-heading">Header</div>
+                <div class="panel-heading">Kullanıcı</div>
                 <div class="panel-body">
                     <h4>123</h4>
-                    <p>Data</p>
+                    <p>Kişi</p>
                 </div>
             </div>
         </div>
