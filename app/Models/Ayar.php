891091412e3680beec512b76fc961607c100721a
topcueser <topcueser@gmail.com>
2018-08-04 10:54:14 +0300

    ayar model ve migration dosyasının olusturulması

---
+++ app/Models/Ayar.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ayar extends Model
+{
+    protected $table = 'ayar';
+    protected $guarded = ['id'];
+    public $timestamps = false;
+}
